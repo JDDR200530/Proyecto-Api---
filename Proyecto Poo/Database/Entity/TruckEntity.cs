14f5c65531a7cb243f6bde11081e446816c3d05a
JDDR200530 <david1970josue@gmail.com>
2024-07-19 16:06:52 -0600

    Primer Commit

---
+++ Proyecto Poo/Database/Entity/TruckEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto_Poo.Database.Entity
+{
+    public class TruckEntity
+    {
+        public Guid TruckId { get; set; }
+
+        public bool Available { get; set; }
+
+        public double MaxWeigth { get; set; }
+    }
+}
